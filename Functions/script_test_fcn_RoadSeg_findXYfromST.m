094b78b53fd8d18ec23738582fae23ee9c0e6282
Craig Beal <craig.beal@gmail.com>
2022-04-01 18:55:35 -0400

    FEAT: updates to test script for finding x,y points from s,t points and map geometry segments

--- Functions/script_test_fcn_RoadSeg_findXYfromST.m
+++ Functions/script_test_fcn_RoadSeg_findXYfromST.m
@@ -5,19 +5,24 @@ close all
 % Enumerate some constants for later convenience and code readability
 E = 1; N = 2;
 
-% Load an example file with just one arc road segment and some objects
-% along the road
+% Load an example file with a file selection dialog
+%ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct;
+% Load an example file from a static file path
 ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_PlotXODR/Data/workzone_50m_curve_objects.xodr');
 
 % Check the structure
 ODRStruct = fcn_RoadSeg_XODRSegmentChecks(ODRStruct);
 
-% Plot the road segment
-fcn_RoadSeg_plotODRRoadGeometry(ODRStruct);
+% Define the max gap between plot points on the road, in meters
+maxRoadGap = 0.1; 
+% Plot the road segment in figure 1 (figure specification optional)
+fcn_RoadSeg_plotODRRoadGeometry(ODRStruct,maxRoadGap);
+% Grab the figure handle for plotting the objects later
 hRoad = gcf;
 
-% Convert the XODR objects to patch objects in an array
+% Define the max gap between points on the object outline, in meters
 maxObjectVertexGap = 0.05;
+% Convert the XODR objects to patch objects in an array
 objectArray = fcn_RoadSeg_convertXODRObjectsToPatchObjects(ODRStruct,maxObjectVertexGap);
 
 % Plot the patch objects on top of the (previously plotted) roadway figure
