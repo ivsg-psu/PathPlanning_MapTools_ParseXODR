8ca29a94cf8d2038af014c43bad24ff332de9d5d
Craig Beal <craig.beal@gmail.com>
2022-05-07 19:34:11 -0400

    FEAT: functionalized the lane extraction process and test script, taking advantage of the PSU path library

--- Functions/script_test_fcn_RoadSeg_extractLaneGeometry.m
+++ Functions/script_test_fcn_RoadSeg_extractLaneGeometry.m
@@ -1,87 +1,92 @@
-% Script to test the parsing of objects from an xODR structure, using the
-% various functions from this library as well as the PlotXODR library and
-% the patch object library (for plotting)
+% Script to test the parsing of lane geometries from an xODR structure,
+% using the various functions from this library as well as the PSU path
+% library (for converting to traversals and plotting)
 
 % This script was written by C. Beal
-% Questions or comments? cbeal@bucknell.edu
+% Questions or comments? sbrennan@psu.edu
 
 % Revision history:
 %     2022_04_01
 %     -- wrote the code
+%     2022-05-07
+%     -- revised the code to use newly created functions for parsing lanes
+%     as well as existing functions within the PSU path library
 
 clearvars
-%close all
-
-% Load an example file with a file selection dialog
-%ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct;
+close all
 
 % Load an example file from a static file path
-%ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/ODRViewerEx.xodr');
-%ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/workzone_150m_double_curve_barrels_repeat.xodr');
-%ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/workzone_50m_curve_objects.xodr');
-%ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/Ex_Simple_Lane_Offset.xodr');
+% Ex_Simple_Lane_Offset
+% ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/Ex_Simple_Lane_Offset.xodr');
+% Ex_Simple_Lane_Offset_Reversed
+% ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/Ex_Simple_Lane_Offset_Reversed.xodr');
+% Ex_Complex_Lane_Offset
 ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/Ex_Complex_Lane_Offset.xodr');
-%ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/workzone_100m_Lane_Offset.xodr');
-%ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/Ex_Simple_Lane_Offset_Reversed.xodr');
+% workzone_100m_Lane_Offset
+% ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/workzone_100m_Lane_Offset.xodr');
 
 % Check the structure
 ODRStruct = fcn_RoadSeg_XODRSegmentChecks(ODRStruct);
 
-% Add the path for the XODR plotting library
-addpath(genpath('/Users/cbeal/Documents/MATLAB/DOT_PlotXODR'));
+% Add the path for the PSU path library
+addpath(genpath('/Users/cbeal/Documents/MATLAB/DOT_PathClassLibrary'))
 
 % Extract the path coordinate lane information from the XODR file for the
 % first road in the specified XODR file
-[sPts,tLeft,tCenter,tRight] = fcn_RoadSeg_extractLaneGeometry(ODRStruct.OpenDRIVE.road{1},0.2);
+[sPts,tLeft,tCenter,tRight] = fcn_RoadSeg_extractLaneGeometry(ODRStruct.OpenDRIVE.road{1},1);
 
-% Determine the number of road geometry segments to plot over
-Nsegments = length(ODRStruct.OpenDRIVE.road{1}.planView.geometry);
+% Obtain the reference line for the road by converting a line with
+% t-coordinate of zero for each of the test station points into (E,N)
+% coordinates
+[xRef,yRef] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRStruct.OpenDRIVE.road{1},sPts,zeros(size(sPts)));
 
-% Iterate over all of the road geometry segments to determine the lane
-% boundaries in (E,N) coordinates
-for segIdx = 1:Nsegments
-  % Determine the
-  segStart = str2double(ODRStruct.OpenDRIVE.road{1}.planView.geometry{segIdx}.Attributes.s);
-  segEnd = segStart + str2double(ODRStruct.OpenDRIVE.road{1}.planView.geometry{segIdx}.Attributes.length);
-  
-  % Determine the indices of the lane station points that lie within each
-  % segment
-  sInds = find(sPts > segStart & sPts <= segEnd);
-  if segIdx == 1 && sInds(1) ~= 1
-    sInds = [1; sInds];
-  end
-  
-  % Convert the path coordinates to obtain the (X,Y) coordinates of each of
-  % the calculated lane boundaries
-  [xCenter(sInds,:),yCenter(sInds,:)] = fcn_RoadSeg_findXYfromSTandSegment(ODRStruct.OpenDRIVE.road{1}.planView.geometry{segIdx},sPts(sInds),tCenter(sInds));
-  for laneIdx = 1:size(tLeft,2)
-    [xLeft(sInds,laneIdx),yLeft(sInds,laneIdx)] = fcn_RoadSeg_findXYfromSTandSegment(ODRStruct.OpenDRIVE.road{1}.planView.geometry{segIdx},sPts(sInds),tLeft(sInds,laneIdx));
-  end
-  for laneIdx = 1:size(tRight,2)
-    [xRight(sInds,laneIdx),yRight(sInds,laneIdx)] = fcn_RoadSeg_findXYfromSTandSegment(ODRStruct.OpenDRIVE.road{1}.planView.geometry{segIdx},sPts(sInds),tRight(sInds,laneIdx));
-  end
+% Determine the number of lanes in the given road
+NlanesL = size(tLeft,2);
+NlanesR = size(tRight,2);
+
+% Preallocate the (X,Y) lane boundary matrices for speed
+xLeft = nan(size(tLeft));
+yLeft = nan(size(tLeft));
+xRight = nan(size(tRight));
+yRight = nan(size(tRight));
+
+% Now convert each of the paths (a two-column matrix of (X,Y) points) into
+% a traversal structure consistent with the PSU path library
+roadRef.traversal{1} = fcn_Path_convertPathToTraversalStructure([xRef yRef]);
+[xCenter,yCenter] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRStruct.OpenDRIVE.road{1},sPts,tCenter);
+laneDataCenter.traversal{1} = fcn_Path_convertPathToTraversalStructure([xCenter yCenter]);
+for laneIdx = 1:NlanesL
+  [xLeft(:,laneIdx),yLeft(:,laneIdx)] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRStruct.OpenDRIVE.road{1},sPts,tLeft(:,laneIdx));
+  laneDataLeft.traversal{laneIdx} = fcn_Path_convertPathToTraversalStructure([xLeft(:,laneIdx) yLeft(:,laneIdx)]);
+end
+for laneIdx = 1:NlanesR
+  [xRight(:,laneIdx),yRight(:,laneIdx)] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRStruct.OpenDRIVE.road{1},sPts,tRight(:,laneIdx));
+  laneDataRight.traversal{laneIdx} = fcn_Path_convertPathToTraversalStructure([xRight(:,laneIdx) yRight(:,laneIdx)]);
 end
 
-% Plot all of the lane lines
-figure(2)
-clf
-hold on
-grid on
-plot(sPts,tCenter,'k--')
-plot(sPts,tLeft,'r--')
-plot(sPts,tRight,'b--')
-xlabel('s coordinate')
-ylabel('t coordinate')
-
-% Overplot the lane lines
+% Plot the lane lines in (E,N) coordinates
 figure(1)
 clf
 hold on
 grid on
-axis auto
-plot(xCenter,yCenter,'k');
-plot(xLeft,yLeft,'r');
-plot(xRight,yRight,'b');
 axis equal
 xlabel('East (m)')
 ylabel('North (m)')
+% Use the PSU path traversals plotting utility to plot the lane boundaries
+fcn_Path_plotTraversalsXY(roadRef,1);
+fcn_Path_plotTraversalsXY(laneDataCenter,1);
+fcn_Path_plotTraversalsXY(laneDataLeft,1);
+fcn_Path_plotTraversalsXY(laneDataRight,1);
+
+% Plot the lane lines in (s,t) coordinates for illustrative/debugging
+% purposes
+figure(2)
+clf
+hold on
+grid on
+hC = plot(sPts,tCenter,'k--','linewidth',1.5);
+hL = plot(sPts,tLeft,'r--','linewidth',1.5);
+hR = plot(sPts,tRight,'b--','linewidth',1.5);
+xlabel('s coordinate')
+ylabel('t coordinate')
+legend([hC hL(1) hR(1)],{'Center Lane','Left Lanes','Right Lanes'})
