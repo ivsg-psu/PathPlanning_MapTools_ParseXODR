45be281aed089bdd3247bb9dc04d9d62224d6ff2
Craig Beal <craig.beal@gmail.com>
2022-05-18 18:42:19 -0400

    FIX: Handling of one-sided lane sections fixed in realistic plotting function

--- Functions/fcn_RoadSeg_plotRealisticRoad.m
+++ Functions/fcn_RoadSeg_plotRealisticRoad.m
@@ -1,4 +1,4 @@
-function fHand = fcn_RoadSeg_plotRealisticRoad(ODRRoad,minPlotGap,varargin)
+function fHand = fcn_RoadSeg_plotRealisticRoad(ODRStruct,minPlotGap,varargin)
 % Script to plot a realistic road environment defined in an XODR file
 %
 % This script was written by C. Beal
@@ -21,56 +21,86 @@ else
   ylabel('North (m)')
 end
 
-% First, extract the lane geometry in order to determine the extents of
-% the road
-[sPts,tLeft,tCenter,tRight] = fcn_RoadSeg_extractLaneGeometry(ODRRoad,minPlotGap);
-% First, find the extents of the road as the maximum t-coordinate on the
-% left side of the road and the minimum t-coordinate on the right side of
-% the road, then add just a tiny bit of
-leftExtents = max(tLeft,[],2,'omitnan');
-rightExtents = min(tRight,[],2,'omitnan');
-% Determine the (E,N) coordinates associated with each of the road
-% extents
-[eLeftExtents,nLeftExtents] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRRoad,sPts,leftExtents);
-[eRightExtents,nRightExtents] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRRoad,sPts,rightExtents);
-% Plot a dark gray patch to represent the road surface
-patch([eLeftExtents; flipud(eRightExtents)],...
-  [nLeftExtents; flipud(nRightExtents)],[0.2 0.2 0.2]);
+% Determine how many roads are defined in the XODR file
+Nroads = length(ODRStruct.OpenDRIVE.road);
 
-% Now, plot the center lane as a double-yellow line, by offsetting the
-% center lane by 10 cm each direction from the center lane definition
-[eCenterLaneL,nCenterLaneL] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRRoad,sPts,tCenter+0.1);
-[eCenterLaneR,nCenterLaneR] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRRoad,sPts,tCenter-0.1);
-plot(eCenterLaneL,nCenterLaneL,'-','linewidth',3,'color',[0.8 0.6 0.1])
-plot(eCenterLaneR,nCenterLaneR,'-','linewidth',3,'color',[0.8 0.6 0.1])
-
-% Finally, determine the outer lines of the driving lanes, assuming that
-% the outside lanes are the shoulder
-[~,inds] = sort(tLeft,2,'descend','MissingPlacement','last');
-for laneIdx = 2:size(tLeft,2)
-  for i = 1:length(sPts)
-    tLeftDrivingBoundary(i,1) = tLeft(i,[inds(i,laneIdx)]);
-  end
-  [eLeftLane,nLeftLane] = ...
-    fcn_RoadSeg_findXYfromSTandODRRoad(ODRRoad,sPts,tLeftDrivingBoundary);
-  if laneIdx == 2
-    plot(eLeftLane,nLeftLane,'-','linewidth',3,'color','white');
+% Iterate through each of the roads in the XODR file and obtain all of the
+% geometry for plotting
+for roadInd = 1:Nroads
+  % Grab a current road object to make it simpler to address it
+  ODRRoad = ODRStruct.OpenDRIVE.road{roadInd};
+  % First, extract the lane geometry in order to determine the extents of
+  % the road
+  [sPts,tLeft,tCenter,tRight] = fcn_RoadSeg_extractLaneGeometry(ODRRoad,minPlotGap);
+  % First, find the extents of the road as the maximum t-coordinate on the
+  % left side of the road and the minimum t-coordinate on the right side of
+  % the road, then add just a tiny bit of
+  if isempty(tLeft)
+    leftExtents = tCenter;
   else
-    plot(eLeftLane,nLeftLane,'--','linewidth',3,'color','white');
+    leftExtents = max(tLeft,[],2,'omitnan');
   end
+  if isempty(tRight)
+    rightExtents = tCenter;
+  else
+    rightExtents = min(tRight,[],2,'omitnan');
+  end
+  % Determine the (E,N) coordinates associated with each of the road
+  % extents
+  [eLeftExtents,nLeftExtents] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRRoad,sPts,leftExtents);
+  [eRightExtents,nRightExtents] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRRoad,sPts,rightExtents);
+  % Plot a dark gray patch to represent the road surface
+  hp = patch([eLeftExtents; flipud(eRightExtents)],...
+    [nLeftExtents; flipud(nRightExtents)],[0.2 0.2 0.2],'edgecolor',[0.2 0.2 0.2]);
 end
 
-% Right side
-[~,inds] = sort(tRight,2,'ascend','MissingPlacement','last');
-for laneIdx = 2:size(tRight,2)
-  for i = 1:length(sPts)
-    tRightDrivingBoundary(i,1) = tRight(i,[inds(i,laneIdx)]);
+% Iterate through each of the roads in the XODR file and obtain all of the
+% geometry for plotting
+for roadInd = 1:Nroads
+  % Grab a current road object to make it simpler to address it
+  ODRRoad = ODRStruct.OpenDRIVE.road{roadInd};
+  % First, extract the lane geometry in order to determine the extents of
+  % the road
+  [sPts,tLeft,tCenter,tRight] = fcn_RoadSeg_extractLaneGeometry(ODRRoad,minPlotGap);
+  % Now, plot the center lane as a double-yellow line, by offsetting the
+  % center lane by 10 cm each direction from the center lane definition
+  [eCenterLaneL,nCenterLaneL] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRRoad,sPts,tCenter+0.1);
+  [eCenterLaneR,nCenterLaneR] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRRoad,sPts,tCenter-0.1);
+  plot(eCenterLaneL,nCenterLaneL,'-','linewidth',3,'color',[0.8 0.6 0.1])
+  plot(eCenterLaneR,nCenterLaneR,'-','linewidth',3,'color',[0.8 0.6 0.1])
+  
+  % Finally, determine the outer lines of the driving lanes, assuming that
+  % the outside lanes are the shoulder
+  if ~isempty(tLeft)
+    [~,inds] = sort(tLeft,2,'descend','MissingPlacement','last');
+    for laneIdx = 2:size(tLeft,2)
+      for i = 1:length(sPts)
+        tLeftDrivingBoundary(i,1) = tLeft(i,[inds(i,laneIdx)]);
+      end
+      [eLeftLane,nLeftLane] = ...
+        fcn_RoadSeg_findXYfromSTandODRRoad(ODRRoad,sPts,tLeftDrivingBoundary);
+      if laneIdx == 2
+        plot(eLeftLane,nLeftLane,'-','linewidth',3,'color','white');
+      else
+        plot(eLeftLane,nLeftLane,'--','linewidth',3,'color','white');
+      end
+    end
   end
-  [eRightLane,nRightLane] = ...
-    fcn_RoadSeg_findXYfromSTandODRRoad(ODRRoad,sPts,tRightDrivingBoundary);
-  if laneIdx == 2
-    plot(eRightLane,nRightLane,'-','linewidth',3,'color','white');
-  else
-    plot(eRightLane,nRightLane,'--','linewidth',3,'color','white');
+  
+  % Right side
+  if ~isempty(tRight)
+    [~,inds] = sort(tRight,2,'ascend','MissingPlacement','last');
+    for laneIdx = 2:size(tRight,2)
+      for i = 1:length(sPts)
+        tRightDrivingBoundary(i,1) = tRight(i,[inds(i,laneIdx)]);
+      end
+      [eRightLane,nRightLane] = ...
+        fcn_RoadSeg_findXYfromSTandODRRoad(ODRRoad,sPts,tRightDrivingBoundary);
+      if laneIdx == 2
+        plot(eRightLane,nRightLane,'-','linewidth',3,'color','white');
+      else
+        plot(eRightLane,nRightLane,'--','linewidth',3,'color','white');
+      end
+    end
   end
 end
--- Functions/script_test_plotRealisticRoad.m
+++ Functions/script_test_plotRealisticRoad.m
@@ -11,11 +11,11 @@ clearvars
 
 % Load an example file from a static file path
 %ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/Ex_Simple_Lane_Offset.xodr');
-ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/Ex_Simple_Lane_Gains.xodr');
+%ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/Ex_Simple_Lane_Gains.xodr');
 %ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/Ex_Simple_Lane_Offset_Reversed.xodr');
 %ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/Ex_Complex_Lane_Offset.xodr');
 %ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/workzone_100m_Lane_Offset.xodr');
-% ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_PlotXODR/Data/ODRViewerEx.xodr');
+ODRStruct = fcn_RoadSeg_convertXODRtoMATLABStruct('/Users/cbeal/Documents/MATLAB/DOT_ParseXODR/Data/ODRViewerEx.xodr');
 
 % Check the structure
 ODRStruct = fcn_RoadSeg_XODRSegmentChecks(ODRStruct);
@@ -35,71 +35,5 @@ Nroads = length(ODRStruct.OpenDRIVE.road);
 % Choose a minimum spacing of the points defining the road geometries
 minPlotGap = 0.2; % (m)
 
-% Iterate through each of the roads in the XODR file and obtain all of the
-% geometry for plotting
-for roadInd = 1:Nroads
-  % First, extract the lane geometry in order to determine the extents of
-  % the road
-  [sPts,tLeft,tCenter,tRight] = fcn_RoadSeg_extractLaneGeometry(ODRStruct.OpenDRIVE.road{roadInd},minPlotGap);
-  % First, find the extents of the road as the maximum t-coordinate on the
-  % left side of the road and the minimum t-coordinate on the right side of
-  % the road, then add just a tiny bit of
-  if isempty(tLeft)
-    leftExtents = tCenter;
-  else
-    leftExtents = max(tLeft,[],2,'omitnan');
-  end
-  if isempty(tRight)
-    rightExtents = tCenter;
-  else
-    rightExtents = min(tRight,[],2,'omitnan');
-  end
-  % Determine the (E,N) coordinates associated with each of the road
-  % extents
-  [eLeftExtents,nLeftExtents] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRStruct.OpenDRIVE.road{roadInd},sPts,leftExtents);
-  [eRightExtents,nRightExtents] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRStruct.OpenDRIVE.road{roadInd},sPts,rightExtents);
-  % Plot a dark gray patch to represent the road surface
-  patch([eLeftExtents; flipud(eRightExtents)],[nLeftExtents; flipud(nRightExtents)],[0.2 0.2 0.2]);
-  
-  % Now, plot the center lane as a double-yellow line, by offsetting the
-  % center lane by 10 cm each direction from the center lane definition
-  [eCenterLaneL,nCenterLaneL] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRStruct.OpenDRIVE.road{roadInd},sPts,tCenter+0.1);
-  [eCenterLaneR,nCenterLaneR] = fcn_RoadSeg_findXYfromSTandODRRoad(ODRStruct.OpenDRIVE.road{roadInd},sPts,tCenter-0.1);
-  plot(eCenterLaneL,nCenterLaneL,'-','linewidth',3,'color',[0.8 0.6 0.1])
-  plot(eCenterLaneR,nCenterLaneR,'-','linewidth',3,'color',[0.8 0.6 0.1])
-  
-  % Finally, determine the outer lines of the driving lanes, assuming that
-  % the outside lanes are the shoulder
-  if ~isempty(tLeft)
-    [~,inds] = sort(tLeft,2,'descend','MissingPlacement','last');
-    for laneIdx = 2:size(tLeft,2)
-      for i = 1:length(sPts)
-        tLeftDrivingBoundary(i,1) = tLeft(i,[inds(i,laneIdx)]);
-      end
-      [eLeftLane,nLeftLane] = ...
-        fcn_RoadSeg_findXYfromSTandODRRoad(ODRStruct.OpenDRIVE.road{roadInd},sPts,tLeftDrivingBoundary);
-      if laneIdx == 2
-        plot(eLeftLane,nLeftLane,'-','linewidth',3,'color','white');
-      else
-        plot(eLeftLane,nLeftLane,'--','linewidth',3,'color','white');
-      end
-    end
-  end
-  
-  % Right side
-  if ~isempty(tRight)
-    [~,inds] = sort(tRight,2,'ascend','MissingPlacement','last');
-    for laneIdx = 2:size(tRight,2)
-      for i = 1:length(sPts)
-        tRightDrivingBoundary(i,1) = tRight(i,[inds(i,laneIdx)]);
-      end
-      [eRightLane,nRightLane] = ...
-        fcn_RoadSeg_findXYfromSTandODRRoad(ODRStruct.OpenDRIVE.road{roadInd},sPts,tRightDrivingBoundary);
-      if laneIdx == 2
-        plot(eRightLane,nRightLane,'-','linewidth',3,'color','white');
-      else
-        plot(eRightLane,nRightLane,'--','linewidth',3,'color','white');
-      end
-    end
-  end
-end
+% Call the plotting function
+fcn_RoadSeg_plotRealisticRoad(ODRStruct,minPlotGap,1);
