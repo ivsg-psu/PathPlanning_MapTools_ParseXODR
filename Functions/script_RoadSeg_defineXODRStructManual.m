6e616b3c8990476ffb072710b898a2e0b244b1b9
Phakphum Artkaew <pakphum82542@gmail.com>
2022-07-19 21:22:38 -0400

    bug fixed in script_RoadSeg_defineXODRStructManual.m

--- Functions/script_RoadSeg_defineXODRStructManual.m
+++ Functions/script_RoadSeg_defineXODRStructManual.m
@@ -422,7 +422,7 @@ axis equal
 xlabel('East (m)')
 ylabel('North (m)')
 % Plot the realistic looking road on the figure
-fcn_RoadSeg_plotRealisticRoad(ODRStruct.OpenDRIVE.road{1},0.5,1);
+fcn_RoadSeg_plotRealisticRoad(ODRStruct,0.5,1);
 
 % Use the axis bounding box to find the extents of the data (this can be
 % replaced by more specific code since this could theoretically miss a
